3add1c1a70547378f9893809b92aa338ff80354f
Mathias <sanzy1207@gmail.com>
2025-05-03 21:46:45 +0200

    Marka Transitionnå ut, might take back later 🐷

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,7 +2,7 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
-
+ /*
   document.addEventListener("DOMContentLoaded", function () {
     document.body.classList.add("fade-in");
 
@@ -19,4 +19,4 @@
         }
       });
     });
-  });
+  }); */
