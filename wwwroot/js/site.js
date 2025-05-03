c0b48f8d3cf9c49822addf99575d8f788036670d
Mathias <sanzy1207@gmail.com>
2025-05-03 21:44:10 +0200

    Transitionmaxx + buttonfix might change transition later 👍
    ya

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,21 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+  document.addEventListener("DOMContentLoaded", function () {
+    document.body.classList.add("fade-in");
+
+    document.querySelectorAll("a.btn").forEach(function (link) {
+      link.addEventListener("click", function (e) {
+        const href = link.getAttribute("href");
+        if (href) {
+          e.preventDefault();
+          document.body.classList.remove("fade-in");
+          document.body.classList.add("fade-out");
+          setTimeout(function () {
+            window.location.href = href;
+          }, 250);
+        }
+      });
+    });
+  });
